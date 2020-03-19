94e917edf4bb6a49cb4756913a984b92d288978b
Alex Dollard <alexdollard141@gmail.com>
2020-03-19 10:32:43 -0500

    added DOB and hometown to cards, imported Moment.js for date formatting

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
+import Moment from 'moment';
 
 function EmployeeCard(props) {
 
   return (
     props.results.map(result => (
 
-    <div className="card">
+    <div className="card"
+          key={result.id.value}>
       <div className="img-container">
         <img alt={result.name} src={result.picture.large} />
       </div>
@@ -20,6 +22,12 @@ function EmployeeCard(props) {
           </li>
           <li>
             <strong>Phone:</strong> {result.phone}
+          </li>
+          <li>
+            <strong>DOB:</strong> {Moment(result.dob.date).format(`MM-DD-YYYY`)}
+          </li>
+          <li>
+            <strong>Location:</strong> {`${result.location.city}, ${result.location.state}`}
           </li>
         </ul>
       </div>
